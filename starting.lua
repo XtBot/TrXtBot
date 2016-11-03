a24daac376e517fc1d867dd52d7a3a81c68bbae8
سید محمد خشنوا <mohamadkhoshnava@users.noreply.github.com>
2016-11-03 15:31:17 +0330

    Add files via upload

---
+++ starting.lua
@@ -0,0 +1,74 @@
+﻿local triggers = {
+		'^/(start)$',
+		'^/(contact) (.+)$',
+		'^/(pm) (%d+)', 
+		'^###cb:/(langfa)',
+		'^###cb:/(langen)'
+}
+	
+local action = function(msg, matches)
+
+if matches[1] == 'start' then
+local text = 'Hi 👋 '..msg.from.first_name..'\nplease select yor language 😊\n-------------\nسلام 👋  '..msg.from.first_name..'\nلطفا زبان خود را انتخاب کنید'
+	keyboard = {} -- Array Of Keyboard
+	keyboard.inline_keyboard = {
+	   {
+			{text = "English", callback_data="/langen"},
+			{text = "Persian", callback_data="/langfa"}
+	   }
+	}
+
+	api.sendChatAction(msg.chat.id, 'typing')
+	api.sendKeyboard(msg.chat.id, text, keyboard, true)
+	end
+	
+	if msg.cb then
+        local text = ''
+        local text1 = ''
+        if matches[1] == 'langfa' then
+			text1 = 'زبان تنظیم شد!'
+            text = 'دوست عزیز '..msg.from.first_name..' به ربات ترجمه خوش اومدی 😊🌹\nبرای ترجمه کلمه یا جمله فقط کافیه متنتو برای ما بفرستی'
+			client:set('UserLnag'..msg.from.id, 'FA')
+        end
+		if matches[1] == 'langen' then
+			text1 = 'Language Set!'
+		    text = [[Welcome my friend 
+please send your text for translate]]
+			client:set('UserLnag'..msg.from.id, 'EN')
+		end
+		api.sendChatAction(msg.chat.id, 'typing')
+        api.editMessageText(msg.chat.id, msg.message_id, text1)
+        api.sendMessage(msg.chat.id, text, true)
+	end
+
+if matches[1] == 'contact' and matches[2] then
+
+api.sendChatAction(msg.chat.id, 'typing')
+api.sendMessage(msg.chat.id, '*Sent!*', true)
+
+api.sendChatAction(msg.chat.id, 'typing')
+api.forwardMessage(config.admin, msg.from.id, msg.message_id)
+end
+
+if is_admin(msg) and matches[1] == 'pm' and matches[2] and matches[3] then
+local is_member = client:sismember('BcUsernames', matches[2])
+
+if is_member then
+api.sendChatAction(matches[2], 'typing')
+api.sendMessage(matches[2], '#New Message\n'..matches[3], true)
+
+api.sendChatAction(msg.chat.id, 'typing')
+api.sendReply(msg, '*Sent For '..matches[2]..'*', true)
+else
+api.sendChatAction(msg.chat.id, 'typing')
+api.sendReply(msg, '*I Can Not Send Any Messages To This User!*', true)
+end
+
+end
+
+end
+
+return {
+  action = action,
+  triggers = triggers,
+}
---
+++ translate.lua
@@ -0,0 +1,82 @@
+﻿local triggers = {
+		'^(.*)$',
+}
+
+local action = function(msg, matches)
+local lang = client:get('UserLnag'..msg.from.id)
+
+if not lang then return false end
+
+if msg.text then
+
+if lang == 'EN' then
+		local keyboard = {}
+        keyboard.inline_keyboard = {
+    	    {
+    		    {text = "Persian", callback_data = '/fa'},
+				{text = "English", callback_data = '/en'},
+	    	},    	    
+			{
+    		    
+				{text = "French", callback_data = '/fr'},
+				{text = "Russian", callback_data = '/ru'},
+	    	},			
+			{
+    		    {text = "Arabic", callback_data = '/ar'},
+				{text = "Chinese", callback_data = '/zh'},
+	    	},
+			{
+				{text = "Japanese", callback_data = '/ja'},
+				{text = "German", callback_data = '/de'},
+	    	},
+			{
+				{text = "Spanish", callback_data = '/es'},
+	    	},			
+			{
+				{text = "Exit", callback_data = '/exit'},
+	    	},
+    	}
+
+client:set('Text'..msg.from.id, URL.escape(msg.text))
+api.sendChatAction(msg.chat.id, 'typing')
+api.sendKeyboard(msg.chat.id, 'Plesae Select Translating Language :', keyboard, true)
+elseif lang == 'FA' then
+		local keyboard = {}
+        keyboard.inline_keyboard = {
+    	    {
+    		    {text = "فارسی", callback_data = '/fa'},
+				{text = "انگلیسی", callback_data = '/en'},
+	    	},    	    
+			{
+				{text = "فرانسوی", callback_data = '/fr'},
+				{text = "روسی", callback_data = '/ru'},
+	    	},			
+			{
+    		    {text = "عربی", callback_data = '/ar'},
+				{text = "چینی", callback_data = '/zh'},
+	    	},
+			{
+				{text = "ژاپنی", callback_data = '/ja'},
+				{text = "آلمانی", callback_data = '/de'},
+	    	},
+			{
+				{text = "اسپانیایی", callback_data = '/es'},
+	    	},
+			{
+				{text = "خروج", callback_data = '/exit'},
+	    	},
+    	}
+
+client:set('Text'..msg.from.id, URL.escape(msg.text))
+api.sendChatAction(msg.chat.id, 'typing')
+api.sendKeyboard(msg.chat.id, 'لطفا زبان ترجمه را انتخاب نمایید :', keyboard, true)
+end
+
+end
+
+end
+	
+return {
+  action = action,
+  triggers = triggers,
+}
